cb0ac4364982877013a99090061de2e172efd709
bad fad <badfad@edu.novia.fi>
2024-01-04 21:25:57 +0200

    updated error loader and implemented PlayPause component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#1b1a1b]">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#0f0a14]">
         <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
